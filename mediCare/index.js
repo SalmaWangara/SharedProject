a8bdf4cc0c50e7428847642581c2862dae64fdca
SalmaWangara <110348649+SalmaWangara@users.noreply.github.com>
2023-03-27 23:32:44 +0300

    Add files via upload

---
+++ mediCare/index.js
@@ -0,0 +1,14 @@
+const login = document.getElementById('Log-in');
+const all = document.getElementById('hero');
+
+login.addEventListener(onClick, () => {
+  all.style.display = block;
+});
+
+
+const hospital = document.querySelector('div.hospitals');
+const cart = document.querySelector('span.cart');
+
+hospital.addEventListener(onClick, () => {
+    
+})
