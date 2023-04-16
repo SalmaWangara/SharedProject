eca1017ef1c6fca569e7af5506fa01cff2c260e8
RayanKW <wairimurayyan@gmail.com>
2023-04-16 17:17:48 +0300

    branch

--- drMedic/env/bin/activate.fish
+++ drMedic/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/pi/projects/drMedic/env"
+set -gx VIRTUAL_ENV "/home/pi/projectDr/SharedProject/drMedic/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
